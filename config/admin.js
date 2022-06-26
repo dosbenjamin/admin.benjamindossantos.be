782f2beb8076a41e1a20243aa3f67d13e17ee01a
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-06-26 22:57:48 +0200

    feat: add api token salt

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '3e03ba4b85ef5f24e8fa77b785e1bba1'),
   },
